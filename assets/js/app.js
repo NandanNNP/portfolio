a888676a3dc97f3658d986f45d3580c2da8e2865
nandan <hello@nandanp.in>
2025-08-02 00:12:33 +0530

    Cvpage edited with pdf

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,7 +27,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="techstack.html">Techstack</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://nandannnp.github.io/Responsible-CV/" target="_blank">CV</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="assets/docs/Nandan%20P%20CV.pdf" target="_blank">CV</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
